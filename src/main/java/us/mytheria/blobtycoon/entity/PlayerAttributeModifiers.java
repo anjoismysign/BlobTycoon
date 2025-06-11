56844daf7ed4c0ce770c2d781e774db98abaf648
Luis Benavides Naranjo <lubensaico@gmail.com>
2025-06-11 12:53:18 -0600

    Refactor PlayerAttributeModifiers to use dynamic attribute registry access

--- src/main/java/us/mytheria/blobtycoon/entity/PlayerAttributeModifiers.java
+++ src/main/java/us/mytheria/blobtycoon/entity/PlayerAttributeModifiers.java
@@ -1,20 +1,31 @@
 package us.mytheria.blobtycoon.entity;
 
+import io.papermc.paper.registry.RegistryAccess;
+import io.papermc.paper.registry.RegistryKey;
+import org.bukkit.Registry;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.entity.Player;
-import org.bukkit.util.NumberConversions;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public record PlayerAttributeModifiers(Map<String, Object> getSerialized) {
 
+    private static List<Attribute> attributes() {
+        RegistryAccess access = RegistryAccess.registryAccess();
+        Registry<@NotNull Attribute> registry = access.getRegistry(RegistryKey.ATTRIBUTE);
+        return registry.stream().toList();
+    }
+
     public static PlayerAttributeModifiers of(Player player) {
         Map<String, Object> map = new HashMap<>();
-        for (Attribute attribute : Attribute.values()) {
+        for (Attribute attribute : attributes()) {
             AttributeInstance instance = player.getAttribute(attribute);
             if (instance == null)
                 continue;
@@ -26,14 +37,17 @@ public record PlayerAttributeModifiers(Map<String, Object> getSerialized) {
                 Map<String, Object> serialized = modifier.serialize();
                 attributeMap.put(modifier.getName(), serialized);
             });
-            map.put(attribute.ordinal() + "", attributeMap);
+            map.put(attribute.getKey().toString(), attributeMap);
         }
         return new PlayerAttributeModifiers(map);
     }
 
     public void apply(Player player) {
         for (Map.Entry<String, Object> entry : getSerialized.entrySet()) {
-            Attribute attribute = Attribute.values()[NumberConversions.toInt(entry.getKey())];
+            String key = entry.getKey();
+            @Nullable Attribute attribute = attributes().stream().filter(x -> x.getKey().toString().equals(key)).findFirst().orElse(null);
+            if (attribute == null)
+                continue;
             Map<String, Object> attributeMap = (Map<String, Object>) entry.getValue();
             for (Map.Entry<String, Object> attributeEntry : attributeMap.entrySet()) {
                 try {
